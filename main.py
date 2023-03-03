2a03825d1fc57f7bf04d046c97f25322147a0182
Autumn Cockrell <autumnjolyn@gmail.com>
2023-03-03 15:08:30 -0500

    set GPIO mode

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import RPi.GPIO as GPIO
 import time
 import pathlib
 import yaml
+import gpiozero
 
 # Read from config.yaml
 with open('config.yml', 'r') as file:
@@ -12,13 +13,21 @@ with open('config.yml', 'r') as file:
 
 # SpiDev init
 spi = spidev.SpiDev()
-# bus,device used to connect to SPI device
-spi.open(bus,device)
+
+# (bus,device) used to connect to SPI device
+# 0,0 is placeholder
+spi.open(0,0)
 
 # 32 MHz MAX
 max_speed_hz = 4800
+# Sets Pin Numbering Declaration (Uses board numbering scheme)
+
+GPIO.setmode(GPIO.BOARD)
 
 # Read from sensors
+# GPIO 9 (G) & 10 for pressure
+
+# GPIO 20 (G) & 21 for thermo
 for x in config['hardware']['sections']:
     spidev.bits_per_word()
 

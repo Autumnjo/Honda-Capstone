0c84567b1c9a0499669b096080c9bab64269e02c
Autumn Cockrell <autumnjolyn@gmail.com>
2023-03-21 04:24:30 -0400

    Added USB check

--- main.py
+++ main.py
@@ -7,13 +7,23 @@ import pathlib
 import yaml
 import pyudev
 
-pressureData = []
-thermoData = []
-
+##### config.yaml file #####
 # Read from config.yaml
 with open('config.yml', 'r') as file:
     config = yaml.safe_load(file)
 
+##### USB reading #####
+# Context object via pyudev for USB reading
+context = pyudev.Context()
+
+# Monitor object for monitoring USB device
+monitor = pyudev.Monitor.from_netlink(context)
+monitor.filter_by(subsystem='usb')
+
+# Begin USB monitoring
+monitor.start()
+
+##### Initialize board/pins #####
 # SpiDev init
 spi = spidev.SpiDev()
 
@@ -60,31 +70,38 @@ GPIO.output(22, 0)
 GPIO.output(26, 0)
 GPIO.output(35, 1)
 
+##### Read data #####
 pressurePin = 21
 thermoPin = 35
+data = []
+usb_present = False
+
 try:
     while True:     # endless loop, press ctrl+c to exit
         # Read from sensors
-        for x in config['hardware']['sections']:
-            # reading 8 bits (1 byte) from pressure sensor
-            pressureRead = spi.xfer2([0x80 | pressurePin, 0x00])
-            pData = ((pressureRead[0] & 0x07) << 8) | pressureRead[1]
-            pressureData.append(pData)
+        device = monitor.poll()
+        if device:
+            if device.action == 'add':
+                usb_present = True
+                while(usb_present):
+                    # reading 8 bits (1 byte) from pressure sensor
+                    pressureRead = spi.xfer2([0x80 | pressurePin, 0x00])
+                    pressure = ((pressureRead[0] & 0x07) << 8) | pressureRead[1]
 
-            df = pd.DataFrame(pressureData, columns=['Pressure_Data'])
-            df.to_csv('Pressure_Data.csv', index=False)
+                    # binary to decimal 150 psi = 1 megapascal 
+                    # in binary, a percentage of the max value, which is 150 psi
+                    thermoRead = spi.xfer2([0x80 | thermoPin, 0x00])
+                    thermo = ((thermoRead[0] & 0x07) << 8) | thermoRead[1]
 
-            # binary to decimal 150 psi = 1 megapascal 
-            # in binary, a percentage of the max value, which is 150 psi
-            thermoRead = spi.xfer2([0x80 | thermoPin, 0x00])
-            tData = ((thermoRead[0] & 0x07) << 8) | thermoRead[1]
-            thermoData.append(tData)
+                    # to .csv
+                    data.append(pressure, thermo)
+                    df = pd.DataFrame(data, columns=['Pressure_Data', 'Thermo_Data'])
+                    df.to_csv('data.csv', index=False)
 
-            df = pd.DataFrame(thermoData, columns=['Thermo_Data'])
-            df.to_csv('Thermo_Data.csv', index=False)
-            
-           # binary value of temp in degrees
-
-            time.sleep(config['software']['poll_rate'])
+                    # sleep for poll rate time
+                    time.sleep(config['software']['poll_rate'])
+                    usb_present = False
+            elif device.action == 'remove' and usb_present:
+                usb_present = False
 finally:
     spi.close()     # close port before exit
